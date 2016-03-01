5c81010f6873d71e90a08521191111a35fbb2991
Lukas Kirchsteiger <kirluc11@htlkaindorf.at>
2016-03-01 14:32:30 +0100

    added function

---
+++ CAC_WP/BL/Function.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAC_WP.BL
+{
+    class Function
+    {
+        private int id { get; set; }
+        private String name { get; set; }
+
+        public void execute()
+        {
+
+        }
+    }
+}
---
+++ CAC_WP/BL/NextFunction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Phone.BackgroundAudio;
+
+namespace CAC_WP.BL
+{
+    class NextFunction : Function
+    {
+        private static NextFunction instance;
+
+        private NextFunction()
+        {
+
+        }
+
+        public static NextFunction getInstance()
+        {
+            if(instance == null)
+            {
+                instance = new NextFunction();
+            }
+            return instance;
+        }
+
+        public new void execute()
+        {
+        }
+    }
+}
